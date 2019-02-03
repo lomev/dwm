38104298f78b43ab57c453134407f28b1bc52baa
reiffarth <ravd@tuta.io>
2019-02-03 11:09:33 +0100

    confih.g fix

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
