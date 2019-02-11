603adedd65e8c17409409944ed3287e6636c85a0
reiffarth <ravd@tuta.io>
2019-02-11 21:28:17 +0100

    Meaningless fix

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int gappx     = 5;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Droid Sans Mono:size=8.5" };
+static const char *fonts[]          = { "Droid Sans Mono:size=9" };
 
 static const char *colors[][3]      = {
 	/*                 fg         bg         border   */
