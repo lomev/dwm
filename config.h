da5f7d0268832472ffb55a9219fe07081bac9292
reiffarth <ravd@tuta.io>
2019-05-10 16:00:39 +0200

    Change config.h settings

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 16;       /* gaps between windows */
-static const unsigned int snap      = 8;       /* snap pixel */
+static const unsigned int gappx     = 32;       /* gaps between windows */
+static const unsigned int snap      = 8;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 8;        /* horizontal padding for statusbar */
