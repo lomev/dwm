2445b24cedc7f4fd8135667ca55cd0cec36d9300
reiffarth <ravd@tuta.io>
2019-05-30 22:16:12 +0200

    Fix no statusbar

--- dwm.c
+++ dwm.c
@@ -731,13 +731,12 @@ drawbar(Monitor *m)
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
-	/* draw status first so it can be overdrawn by tags later
-	if (m == selmon) {  status is only drawn on selected monitor
+	/* draw status first so it can be overdrawn by tags later */
+	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
         sw = TEXTW(stext);
        	drw_text(drw, m->ww - sw, 0, sw, bh, lrpad / 2, stext, 0);
 	}
-    */
 
 	for (c = m->clients; c; c = c->next) {
 		occ |= c->tags == 255 ? 0 : c->tags;
