92f8a7464abd43c5a9aa6850e0f8a3c65d1fdad0
lomev <lomev@disroot.org>
2020-04-03 00:18:45 +0200

    Update config

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE f/le for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 16;       /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 8;       /* gaps between windows */
 static const unsigned int snap      = 8;        /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 3;   /* systray spacing */
@@ -10,9 +10,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizpadbar        = 1;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 6;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 4;        /* vertical padding for statusbar */
+static const char *fonts[]          = { "monospace:size=9" };
 
 static char normbordercolor[] = "#444444";
 static char normbgcolor[]     = "#222222";
@@ -37,8 +37,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor    float x,y,w,h     floatborderpx */
-    // { "sxiv",     NULL,       NULL,       0,            1,           -1 },
-    // { "zathura",  NULL,       NULL,       0,            1,           -1 },
+    { "sxiv",     NULL,       NULL,       0,            1,           -1,        30,30,320,240,    1 },
+    { "zathura",  NULL,       NULL,       0,            1,           -1,        30,30,320,240,    1 },
     { "mpv",      NULL,       NULL,       0,            1,           -1,        30,30,320,240,    4 },
     { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
@@ -104,7 +104,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,           XK_j,         pushdown,       {0} },
 	{ MODKEY|ShiftMask,           XK_k,         pushup,         {0} },
 	{ MODKEY,                     XK_i,         incnmaster,     {.i = +1 } },
-	{ MODKEY,                     XK_u,         incnmaster,     {.i = -1 } },
+	{ MODKEY,                     XK_d,         incnmaster,     {.i = -1 } },
 	{ MODKEY,                     XK_h,         setmfact,       {.f = -0.05} },
 	{ MODKEY,                     XK_l,         setmfact,       {.f = +0.05} },
 	{ MODKEY,                     XK_x,         transfer,       {0} },
