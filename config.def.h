099a7252ed99c44f2fc9727e8132be23f2adfb5d
lomev <lomev@disroot.org>
2020-05-13 12:52:18 +0200

    Show every tag even if there is no client there

--- config.def.h
+++ config.def.h
@@ -29,7 +29,7 @@ static char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -40,7 +40,7 @@ static const Rule rules[] = {
     { "sxiv",     NULL,       NULL,       0,            1,           -1,        30,30,320,240,    1 },
     { "zathura",  NULL,       NULL,       0,            1,           -1,        30,30,320,240,    1 },
     { "mpv",      NULL,       NULL,       0,            1,           -1,        30,30,320,240,    4 },
-    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,        30,30,320,240,    1 },
+    { "Firefox",  NULL,       NULL,       1 << 4,       0,           -1,        30,30,320,240,    1 },
 };
 
 /* layout(s) */
--- dwm.c
+++ dwm.c
@@ -502,7 +502,7 @@ attachstack(Client *c)
 void
 buttonpress(XEvent *e)
 {
-	unsigned int i, x, click, occ = 0;
+	unsigned int i, x, click;
 	Arg arg = {0};
 	Client *c;
 	Monitor *m;
@@ -517,13 +517,9 @@ buttonpress(XEvent *e)
 	}
 	if (ev->window == selmon->barwin) {
 		i = x = 0;
-		for (c = m->clients; c; c = c->next)
-			occ |= c->tags == 255 ? 0 : c->tags;
-		do {
-			if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-				continue;
+        do
 			x += TEXTW(tags[i]);
-		} while (ev->x >= x && ++i < LENGTH(tags));
+		while (ev->x >= x && ++i < LENGTH(tags));
 		if (i < LENGTH(tags)) {
 			click = ClkTagBar;
 			arg.ui = 1 << i;
@@ -855,6 +851,8 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, mid, sw = 0, stw = 0;
+    int boxs = drw->fonts->h / 9;
+   	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
@@ -870,18 +868,19 @@ drawbar(Monitor *m)
 
 	resizebarwin(m);
 	for (c = m->clients; c; c = c->next) {
-		occ |= c->tags == 255 ? 0 : c->tags;
+		occ |= c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
-		if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-			continue;
-	
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+		if (occ & 1 << i)
+			drw_rect(drw, x + boxs, boxs, boxw, boxw,
+				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
+				urg & 1 << i);
 		x += w;
 	}
 
